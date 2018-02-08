ab3897e4e7edd8d2981666e515ae1e322434272a
likehan <kehanli2@illinois.edu>
2018-02-07 23:40:44 -0600

    Initial project commit.

--- src/WebScaper.java
+++ src/WebScaper.java
@@ -1,5 +1,34 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
 public class WebScaper {
-    public static void main(String[] unused) {
-        System.out.println("Hello");
+    /**
+     * Retrieve contents from a URL and return them as a string.
+     *
+     * @param url url to retrieve contents from
+     * @return the contents from the url as a string, or an empty string on error
+     */
+    public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
+    }
+
+    /**
+     *Webscraper
+     * @param unused unused input arguments
+     */
+    public static void main(final String[] unused) {
+        Scanner lineScanner = new Scanner(System.in);
+        System.out.println("Enter a URL: ");
+        String first = lineScanner.nextLine();
+        System.out.println(urlToString(first));
     }
 }
